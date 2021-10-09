37d479d64f21b3ee45e619a4f91e7edeed82da0e
Brandyn <hufferbrandyn@gmail.com>
2021-10-08 20:15:10 -0500

    add question prompts for user and function for rendering badges for licenses

--- index.js
+++ index.js
@@ -1,4 +1,59 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
-const readMeOutline = require('./readMeOutline');
+
+const questions = () => {
+    return inquirer.prompt([
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project titled?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Write a brief description."
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "How to install this project?",
+    },
+    {
+        type: "input",
+        name: "useage",
+        message: "What is this used for?",
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Who all contributed to this project?",
+    },
+    {
+        type: "input",
+        name: "testInstruction",
+        message: "What are test instructions?",
+    },
+    {
+        type: "input",
+        name: "gitHub",
+        message: "What is your GitHub username for questions?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email for Questions?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Using the arrow keys, please select a license to include:",
+        choices: [
+            "Apache License 2.0",
+            "GNU General Public License v3.0",
+            "MIT License",
+            "Mozilla Public License 2.0",
+        ]
+    },
+])
+};
 
--- readMeOutline.js
+++ readMeOutline.js
@@ -1,35 +1,66 @@
-  const generateReadMe = ({ title, description, installation, usage, testInstruction, gitHub, email, contributors, data }) =>  {
-    return `# ${title}
-    
-    ## Table of Contents
-    - [Description](#description)
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [Test Instructions](#test-instructions)
-    - [Questions](#questions)
-    - [Contributors](#contributors)
-    - [License](#license)
-    
-    ## Description 
-    ${description}
-    
-    ## Installation
-    ${installation}
-    
-    ## Usage
-    ${usage}
-    
-    ## Test-Instructions
-    ${testInstruction}
-    
-    ## Questions
-    Github: ${gitHub}
-    E-mail: ${email}
-    
-    ## Contributors
-    ${contributors}
-    
-  `;
-  }
+// Function to generate generic readMe
 
-module.exports = readMeOutline;
+const generateReadMe = (data) => {
+    return`# ${data.title}
+    
+## Table of Contents
+
+- [Description](#description)
+
+- [Installation](#installation)
+
+- [Usage](#usage)
+
+- [Test Instructions](#test-instructions)
+
+- [Questions](#questions)
+
+- [Contributors](#contributors)
+
+- [License](#license)
+
+## Description 
+${data.description}
+
+## Installation
+${data.installation}
+    
+## Usage
+${data.useage}
+    
+## Test-Instructions
+${data.testInstruction}
+    
+## Questions
+- Github: ${data.gitHub}
+- E-mail: ${data.email}
+    
+## Contributors
+${data.contributors}
+    
+## License
+${includeLicenseBadge(data)}
+`;
+
+}
+
+// Renders badge for license user chooses to include
+
+function includeLicenseBadge(data) {
+    let licenseType = `${data.license}`;
+    let theLicense = '';
+    if(licenseType === 'MIT License') {
+      theLicense = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+    } else if (licenseType === 'GNU General Public License v3.0') {
+      theLicense = `[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)`
+    } else if (licenseType === 'Apache License 2.0') {
+      theLicense = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+    } else if (licenseType === 'Mozilla Public License 2.0') {
+      theLicense = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+    } else {
+      licenseType = "N/A"
+    };
+    return theLicense;
+  };
+
+module.exports = generateReadMe;
